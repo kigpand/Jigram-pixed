9f6fb8ae01adf5ab745e0783f84d3892771d1ded
kigpand <kigpand@naver.com>
2021-09-30 19:56:40 +0900

    backend url 수정

--- JigramFront/config/config.js
+++ JigramFront/config/config.js
@@ -1,3 +1,3 @@
 // export const backUrl = 'http://api.jigram.co.kr';
 export const url = 'http://localhost:3000';
-export const backUrl = 'http://localhost:3080';
+export const backUrl = 'http://3.34.196.11';
